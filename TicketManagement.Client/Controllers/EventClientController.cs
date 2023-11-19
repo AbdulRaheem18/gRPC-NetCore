b39ed58d63cd0b4b61295829c34b50c432953bcb
Abdul Raheem <30385993+AbdulRaheem18@users.noreply.github.com>
2023-11-19 18:25:18 +0200

    Added the client gRPC

---
+++ TicketManagement.Client/Controllers/EventClientController.cs
@@ -0,0 +1,30 @@
+﻿using EventCatalog.gRPC;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using static EventCatalog.gRPC.EventCatalogService;
+
+namespace TicketManagement.Client.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventClientController : Controller
+    {
+        private readonly string _serviceURL;
+
+        public EventClientController(IConfiguration IConfig)
+        {
+            _serviceURL = IConfig["ServiceURL"];
+        }
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            //calling grpc client 
+
+            var channel = GrpcChannel.ForAddress(_serviceURL);
+            var client = new EventCatalogServiceClient(channel);
+            var request = new GetCategoriesRequest();
+            var response = client.GetAllCategories(request);
+            return Ok(response);
+        }
+    }
+}
